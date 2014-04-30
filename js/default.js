6ae6968874f79b274041bcb2cfa8d221575f92ea
Simon Hughesdon <simon@hughesdon.com>
2014-04-30 15:06:28 +0100

    minify css / js

--- js/default.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="../scripts/jquery-2.1.0.js" />
-/// <reference path="../default.html" />
-
-var app = function (){
-    var upperSopsCount = form.uppersops.value;
-    alert(upperSopsCount);
-};
-
-app();
